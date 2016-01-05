1ec0eb39b866e8789c44c05784407e5e6fca9af6
Gaines Kergosien <github@kergosien.net>
2016-01-05 12:05:41 -0800

    Completed Scala Exercise

--- scala/src/main/scala/PrimeFactors.scala
+++ scala/src/main/scala/PrimeFactors.scala
@@ -2,8 +2,22 @@ package com.crebma.primeFactors
 
 object PrimeFactors {
 
-  def factors(num: Int) : List[Int] = {
-    List[Int]()
+  def factors(num: Int, c: Int=2) : List[Int] = {
+    if (num == 1) {
+      List[Int]()
+    }
+    else {
+      if (num > c)
+        if (num % c == 0) {
+          List[Int](c) ++ factors(num/c, c)
+        }
+        else {
+          factors(num, c+1)
+        }
+      else
+      {
+        List[Int](num)
+      }
+    }
   }
-
 }
