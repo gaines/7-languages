1042cdc381cc44cd128b47bc21c754c0c6cace7a
Gaines Kergosien <github@kergosien.net>
2016-01-05 08:44:49 -0800

    Completed Haskell Exercise

--- haskell/PrimeFactors.hs
+++ haskell/PrimeFactors.hs
@@ -1,4 +1,11 @@
 module PrimeFactors (primeFactors) where
 
-primeFactors :: Integer -> [Integer]
-primeFactors x = []
+  primeFactors :: Integer -> [Integer]
+  primeFactors 1 = []
+  primeFactors x = primeFactorsRecursive x 2
+
+  primeFactorsRecursive :: Integer -> Integer -> [Integer]
+  primeFactorsRecursive x c
+    | x < c = []
+    | (x `mod` c == 0) = [c] ++ primeFactorsRecursive (x `div` c) c
+    | otherwise = primeFactorsRecursive x (c+1)
