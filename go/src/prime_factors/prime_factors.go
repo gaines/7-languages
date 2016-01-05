46399e69dd50006f97c9af059cd387e6451c11b8
Gaines Kergosien <github@kergosien.net>
2016-01-05 13:47:44 -0800

    Completed Go Exercise

--- go/src/prime_factors/prime_factors.go
+++ go/src/prime_factors/prime_factors.go
@@ -1,5 +1,21 @@
 package prime_factors
 
 func Factors(num int) []int {
-  return []int{}
+  if num == 1 {
+    return []int{}
+  } else {
+    return FactorsExt(num, 2)
+  }
+}
+
+func FactorsExt(num int, c int) []int {
+  if (num <= c) {
+    return []int{num}
+  }
+
+  if (num % c == 0) {
+    return append([]int{c}, FactorsExt(num / c, c)...)
+  } else {
+    return FactorsExt(num, c + 1)
+  }
 }
