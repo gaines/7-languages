1a86a1d77cb362f026716737c28a1c61fbd6b39d
Gaines Kergosien <github@kergosien.net>
2016-01-05 12:51:12 -0800

    Completed Elixir Exercise

--- elixir/prime_factors/lib/pf/prime_factors.ex
+++ elixir/prime_factors/lib/pf/prime_factors.ex
@@ -1,5 +1,14 @@
 defmodule PF.PrimeFactors do
-  def prime_factors(num) do
-    []
+  def prime_factors(num, c \\ 2) do
+    cond do
+      num == 1 -> []
+      num > c ->
+        if (rem(num, c) == 0) do
+          [c] ++ prime_factors(div(num, c), c)
+        else
+          prime_factors(num, c + 1)
+        end
+      true -> [num]
+    end
   end
 end
